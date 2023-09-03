dde531f8a4b2dcd8c17b6f9fec00c1a0035abcd5
Aggerio <agamsrivastava004@gmail.com>
2023-09-04 00:55:06 +0530

    Trying different configs

--- backend/App.py
+++ backend/App.py
@@ -1,34 +1,59 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
+# from flask_socketio import SocketIO
+
+# app = Flask(__name__)
+# cors = CORS(app)
+# socketio = SocketIO(app, cors_allowed_origins='*')
+
+# @app.route("/offer", methods=["POST"])
+# def offer():
+#     data = request.json
+#     # Process the offer data and create an answer
+#     # Send the answer back to the React component
+#     print(f"received: {data}")
+#     answer_data = "YUPPP"
+#     return jsonify(answer_data)
+
+# @app.route("/answer", methods=["POST"])
+# def answer():
+#     data = request.json
+#     return "OK"
+
+# @socketio.on('stream')
+# def handle(data):
+#     print(f"Connected and got data\n\n {data}")
+
+# if __name__ == "__main__":
+#     # app.run(host="localhost", port=5000)
+#     socketio.run(app, port=5000)
+
+
+
+from flask import Flask, render_template
 from flask_socketio import SocketIO
+from flask_cors import CORS
+import cv2
 
 app = Flask(__name__)
 cors = CORS(app)
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-@app.route("/offer", methods=["POST"])
-def offer():
-    data = request.json
-    # Process the offer data and create an answer
-    # Send the answer back to the React component
-    print(f"received: {data}")
-    answer_data = "YUPPP"
-    return jsonify(answer_data)
+@socketio.on("connect")
+def handle_connect():
+    print("Client connected")
 
-@app.route("/answer", methods=["POST"])
-def answer():
-    data = request.json
-    # Process the answer from the React component
-    # Establish the WebRTC connection
-    return "OK"
+@socketio.on("disconnect")
+def handle_disconnect():
+    print("Client disconnected")
 
+@socketio.on("video_frame")
+def handle_video_frame(frame_blob):
+    # Handle the received video frame blob (e.g., save it to disk, process it)
+    print(f"received: {frame_blob}")
 
-
-@socketio.on('connect')
-def handle():
-    print("Connected")
+    with open("received_frame.webm", "wb") as f:
+        f.write(frame_blob)
 
 if __name__ == "__main__":
-    # app.run(host="localhost", port=5000)
-    socketio.run(app, port=5000)
-
+    socketio.run(app, host="localhost", port=5000)
--- frontend/src/components/VideoFeed.jsx
+++ frontend/src/components/VideoFeed.jsx
@@ -1,60 +1,172 @@
-import React from "react";
+// import React, { useState, useRef, useEffect } from "react";
+// import { io } from "socket.io-client";
 
-class VideoFeed extends React.Component {
-  constructor(props) {
-    super(props);
-    this.streamCamVideo = this.streamCamVideo.bind(this)
+// const VideoFeed = () => {
+//   const videoRef = useRef(null);
+//   const [mediaStream, setMediaStream] = useState(null);
+
+
+//   function removeAudioFromStream(originalStream) {
+//     const videoTracks = originalStream.getVideoTracks();
+
+//     // Create a new MediaStream with only video tracks
+//     const videoOnlyStream = new MediaStream();
+//     videoTracks.forEach((track) => {
+//       videoOnlyStream.addTrack(track);
+//     });
+
+//     return videoOnlyStream;
+//   }
+
+//   const streamCamVideo = () => {
+//     const constraints = { audio: true, video: { width: 1920, height: 1080 } };
+//     navigator.mediaDevices
+//       .getUserMedia(constraints)
+//       .then((stream) => {
+//         videoRef.current.srcObject = removeAudioFromStream(stream);
+//         videoRef.current.removeAttribute("controls");
+//         videoRef.current.removeAttribute("audio");
+//         videoRef.current.onloadedmetadata = (e) => {
+//           videoRef.current.play();
+//         };
+//         setMediaStream(stream);
+//       })
+//       .catch((err) => {
+//         console.log(err.name + ": " + err.message);
+//       });
+//   };
+
+//   const stopCamVideo = () => {
+//     if (videoRef.current) {
+//       videoRef.current.pause();
+//     }
+//   };
+
+//   const playCamVideo = () => {
+//     if (videoRef.current) {
+//       videoRef.current.play();
+//     }
+//   };
+
+//   const endCamVideo = () => {
+//     if (mediaStream) {
+//       const tracks = mediaStream.getTracks();
+//       tracks.forEach((track) => {
+//         track.stop();
+//       });
+//       videoRef.current.srcObject = null;
+//     }
+//   };
+
+//   useEffect(() => {
+//     // Perform any additional setup or side effects here
+//     if (mediaStream != null) {
+
+//       const socket = io("http://127.0.0.1:5000");
+
+//       socket.on("connect", () => {
+//         console.log(socket.connected);
+//       })
+
+//       socket.on("disconnect", () => {
+//         console.log(socket.connected);
+
+//       })
+
+//       var string = "Hello";
+//       socket.emit("stream", string);
+//       console.log(mediaStream);
+//     }
+//     return () => {
+//       // Cleanup or remove any event listeners if needed
+//     };
+//   },);
+
+//   return (
+//     <div>
+//       <div id="container">
+//         <video autoPlay={true} ref={videoRef}></video>
+//       </div>
+//       <br />
+//       <button onClick={streamCamVideo}>Start streaming</button>
+//       <button onClick={stopCamVideo}>Pause streaming</button>
+//       <button onClick={playCamVideo}>Continue streaming</button>
+//       <button onClick={endCamVideo}>End streaming</button>
+//     </div>
+//   );
+// };
+
+// export default VideoFeed;
+
+
+import React, { useEffect, useRef, useState } from "react";
+import io from "socket.io-client";
+
+function VideoStreaming() {
+
+  const videoRef = useRef(null);
+  const [mediaStream, setMediaStream] = useState(null);
+  const [socket, setSocket] = useState(io("http://localhost:5000"));
+  function removeAudioFromStream(originalStream) {
+    const videoTracks = originalStream.getVideoTracks();
+
+    // Create a new MediaStream with only video tracks
+    const videoOnlyStream = new MediaStream();
+    videoTracks.forEach((track) => {
+      videoOnlyStream.addTrack(track);
+    });
+
+    return videoOnlyStream;
   }
-  streamCamVideo() {
-    var constraints = { audio: true, video: { width: 1920, height: 1080 } };
+
+  const streamCamVideo = () => {
+
+    const constraints = { audio: true, video: { width: 1920, height: 1080 } };
     navigator.mediaDevices
       .getUserMedia(constraints)
-      .then(function(mediaStream) {
-        // var video = document.querySelector("video");
-        var video = document.getElementById("videoElement")
-
-        video.srcObject = mediaStream;
-        video.removeAttribute("controls");
-        video.onloadedmetadata = function(e) {
-          video.play();
+      .then((stream) => {
+        videoRef.current.srcObject = removeAudioFromStream(stream);
+        videoRef.current.removeAttribute("controls");
+        videoRef.current.onloadedmetadata = (e) => {
+          videoRef.current.play();
         };
+        setMediaStream(stream);
       })
-      .catch(function(err) {
+      .catch((err) => {
         console.log(err.name + ": " + err.message);
-      }); // always check for errors at the end.
-  }
-  stopCamVideo() {
-    var video = document.getElementById("videoElement");
-    video.pause();
-  }
-  playCamVideo() {
-    var video = document.getElementById("videoElement");
-    video.play();
-  }
-  EndCamVideo() {
-    var videoEl = document.getElementById('videoElement');
-    var stream = videoEl.srcObject;
-    var tracks = stream.getTracks();
-    tracks.forEach(function(track) {
-      // stopping every track
-      track.stop();
-    });
-    videoEl.srcObject = null;
-  }
-  render() {
-    return (
-      <div>
-        <div id="container">
-          <video autoPlay={true} id="videoElement" ></video>
-        </div>
-        <br />
-        <button onClick={this.streamCamVideo}>Start streaming</button>
-        <button onClick={this.stopCamVideo}>Pause streaming</button>
-        <button onClick={this.playCamVideo}>Continue streaming</button>
-        <button onClick={this.EndCamVideo}>End streaming</button>
-      </div>
-    );
-  }
+      });
+  };
+  function sendVideoFrame() {
+    var canvas = document.getElementById("canvas");
+    canvas.width = 1920;
+    canvas.height = 1080;
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+    canvas.toBlob((blob) => {
+      socket.emit("video_frame", blob);
+    }, "video/webm");
+    requestAnimationFrame(sendVideoFrame);
+  };
+  useEffect(() => {
+    // Create a WebSocket connection to the Flask server
+    if (mediaStream) {
+      // sendVideoFrame();
+    }
+    return () => {
+      // Disconnect from the WebSocket server when unmounting the component
+      // if (socket.current) {
+      //   socket.current.disconnect();
+      // }
+    };
+  }, []);
+
+  return (
+    <div id="container">
+      {/* <canvas id="canvas" hidden={true} /> */}
+      <video autoPlay={true} ref={videoRef} />
+      <button onClick={streamCamVideo} />
+    </div>
+  );
 }
 
-export default VideoFeed;
+export default VideoStreaming;
